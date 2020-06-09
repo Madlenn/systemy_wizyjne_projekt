332fea9e80592f6efd4400158348edfa2d1d6ccd
Madlenn <szymanek.majka@gmail.com>
2020-06-09 12:34:53 +0200

    tried to add library but...wtf

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -1,10 +1,9 @@
 package com.company;
+compile group: 'net.sourceforge.tess4j', name: 'tess4j', version: '1.3.0'
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 
 
@@ -12,12 +11,11 @@ public class MyFrame extends JFrame {
     private String imagePath;
 
     public MyFrame() {
-        //super(("Not Hello world"));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1200, 800);
         setLocation(300, 90);
         //setLayout(new FlowLayout(FlowLayout.CENTER));
-        setLayout (new FlowLayout());
+        setLayout(new FlowLayout());
         JButton pobierz = new JButton("pobierz");
         JButton gray = new JButton("gray");
         JButton scale = new JButton("scale");
@@ -37,149 +35,162 @@ public class MyFrame extends JFrame {
         JButton negative = new JButton("negative");
 
         add(pobierz);
-        add(gray);
-        add(scale);
-        add(rotate);
-        add(blur);
-        add(filter2D);
-        add(sqrBox);
-        add(light);
-        add(edgeDetection);
-        add(erosion);
-        add(dilate);
-        add(brightness);
-        add(skeletonization);
-        add(thresholding);
-        add(histogram);
-        add(cc);
-        add(negative);
+
 
         JLabel label = new JLabel(); //labelka z oryginalnym obrazkiem
+
         JLabel label2 = new JLabel();
         add(label);
         add(label2);
 
-        pobierz.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser file = new JFileChooser();
-                file.setCurrentDirectory(new File(System.getProperty("user.home")));
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images", "jpeg");
-                file.addChoosableFileFilter(filter);
-                int result = file.showSaveDialog(null);
-                if (result == JFileChooser.APPROVE_OPTION) {
+        pobierz.addActionListener(e -> {
+            JFileChooser file = new JFileChooser();
+            file.setCurrentDirectory(new File(System.getProperty("user.home")));
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("JPEG files", "jpg", "jpeg");
+
+
+            file.addChoosableFileFilter(filter);
+            int result = file.showSaveDialog(null);
+
+            if (result == JFileChooser.APPROVE_OPTION) {
+                String extension = getFileExtension(file);
+                if (!extension.equals(".jpeg")) {
+
+                    JOptionPane.showMessageDialog(null, "wybrałeś niepoprawny plik, wybierz .jpeg");
+
+                } else {
+                    String a = getFileExtension(file);
+                    System.out.println(a);
                     File selectedFile = file.getSelectedFile();
                     imagePath = selectedFile.getAbsolutePath();
 
                     label.setIcon(new ScaleImageConverter(imagePath, 400, 300).scaleImage());
                     label.setHorizontalAlignment(JLabel.CENTER);
                     label.setVerticalAlignment(JLabel.CENTER);
-
-                } else if (result == JFileChooser.CANCEL_OPTION) {
-                    System.out.println("No File Select");
+                    add(gray);
+                    add(scale);
+                    add(rotate);
+                    add(blur);
+                    add(filter2D);
+                    add(sqrBox);
+                    add(light);
+                    add(edgeDetection);
+                    add(erosion);
+                    add(dilate);
+                    add(brightness);
+                    add(skeletonization);
+                    add(thresholding);
+                    add(histogram);
+                    add(cc);
+                    add(negative);
                 }
-            }
-        });
-        gray.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new GreyImageConverter(imagePath, 400, 300).toGrayImageIcon();
-                label.setIcon(image);
-            }
-        });
-        scale.addActionListener(new ActionListener() {
 
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new ScaleImageConverter(imagePath, 100, 100).scaleImage();
-                label.setIcon(image);
+            } else if (result == JFileChooser.CANCEL_OPTION) {
+                System.out.println("No File Select");
             }
         });
-        rotate.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new RotateImageConverter(imagePath, 400, 300).rotateImageIcon(90);
-                label.setIcon(image);
-            }
+        gray.addActionListener(e -> {
+            ImageIcon image = new GreyImageConverter(imagePath, 400, 300).toGrayImageIcon();
+            label.setIcon(image);
         });
-        blur.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new FilterImageConverter(imagePath, 400, 300).blurImageIcon();
-                label.setIcon(image);
-            }
+        scale.addActionListener(e -> {
+            ImageIcon image = new ScaleImageConverter(imagePath, 100, 100).scaleImage();
+            label.setIcon(image);
         });
-        filter2D.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new FilterImageConverter(imagePath, 400, 300).filter2DImageIcon();
-                label.setIcon(image);
-            }
+        rotate.addActionListener(e -> {
+            ImageIcon image = new RotateImageConverter(imagePath, 400, 300).rotateImageIcon(90);
+            label.setIcon(image);
         });
-        sqrBox.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new FilterImageConverter(imagePath, 400, 300).sqrBoxFilterImageIcon();
-                label.setIcon(image);
-            }
+        blur.addActionListener(e -> {
+            ImageIcon image = new FilterImageConverter(imagePath, 400, 300).blurImageIcon();
+            label.setIcon(image);
         });
-        light.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new FilterImageConverter(imagePath, 400, 300).lightFilterImageIcon();
-                label.setIcon(image);
-            }
+        filter2D.addActionListener(e -> {
+            ImageIcon image = new FilterImageConverter(imagePath, 400, 300).filter2DImageIcon();
+            label.setIcon(image);
         });
-        edgeDetection.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new EdgeDetection(imagePath, 400, 300).edgeDetection();
-                label.setIcon(image);
-            }
+        sqrBox.addActionListener(e -> {
+            ImageIcon image = new FilterImageConverter(imagePath, 400, 300).sqrBoxFilterImageIcon();
+            label.setIcon(image);
         });
-        erosion.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new ErosionDilation(imagePath, 400, 300).erosion();
-                label.setIcon(image);
-            }
+        light.addActionListener(e -> {
+            ImageIcon image = new FilterImageConverter(imagePath, 400, 300).lightFilterImageIcon();
+            label.setIcon(image);
         });
-        dilate.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new ErosionDilation(imagePath, 400, 300).dilation();
-                label.setIcon(image);
-            }
+        edgeDetection.addActionListener(e -> {
+            ImageIcon image = new EdgeDetection(imagePath, 400, 300).edgeDetection();
+            label.setIcon(image);
         });
-        brightness.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new Brightness(imagePath, 400, 300).brightness();
-                label.setIcon(image);
-            }
+        erosion.addActionListener(e -> {
+            ImageIcon image = new ErosionDilation(imagePath, 400, 300).erosion();
+            label.setIcon(image);
         });
-        skeletonization.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new Skeletonization(imagePath, 400, 300).skeletonization();
-                label.setIcon(image);
-            }
+        dilate.addActionListener(e -> {
+            ImageIcon image = new ErosionDilation(imagePath, 400, 300).dilation();
+            label.setIcon(image);
         });
-        thresholding.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new Thresholding(imagePath, 400, 300).thresholding();
-                label.setIcon(image);
-            }
+        brightness.addActionListener(e -> {
+            ImageIcon image = new Brightness(imagePath, 400, 300).brightness();
+            label.setIcon(image);
         });
-        histogram.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new Histogram(imagePath, 400, 300).histogram();
-                ImageIcon image2 = new Histogram(imagePath, 400, 300).histogram();
-                //label.setIcon(image);
-                label2.setIcon(image2);
-            }
+        skeletonization.addActionListener(e -> {
+            ImageIcon image = new Skeletonization(imagePath, 400, 300).skeletonization();
+            label.setIcon(image);
         });
-        cc.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new ColorSpaceConversion(imagePath, 400, 300).cc();
-                label.setIcon(image);
-            }
+        thresholding.addActionListener(e -> {
+            ImageIcon image = new Thresholding(imagePath, 400, 300).thresholding();
+            label.setIcon(image);
         });
-        negative.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                ImageIcon image = new Negative(imagePath, 400, 300).negative();
-                label.setIcon(image);
-            }
+        histogram.addActionListener(e -> {
+            ImageIcon image = new Histogram(imagePath, 400, 300).histogram();
+            ImageIcon image2 = new Histogram(imagePath, 400, 300).histogram();
+            //label.setIcon(image);
+            label2.setIcon(image2);
+        });
+        cc.addActionListener(e -> {
+            ImageIcon image = new ColorSpaceConversion(imagePath, 400, 300).cc();
+            label.setIcon(image);
+        });
+        negative.addActionListener(e -> {
+            ImageIcon image = new Negative(imagePath, 400, 300).negative();
+            label.setIcon(image);
         });
         setVisible(true);
     }
 
+    private String getFileExtension(JFileChooser file) {
+        String extension = "";
+        try {
+            if (file != null) {
+
+                String name = file.getSelectedFile().getName();
+
+                extension = name.substring(name.lastIndexOf("."));
+            }
+        } catch (Exception e) {
+            extension = " ";
+        }
+
+        return extension;
+
+    }
+
+
+/*    private String getFileExtension(JFileChooser file) {
+        String extension = "";
+        try {
+            if (file != null ) {
+
+                String name = file.getSelectedFile().getName();
+                System.out.println(name);
+                extension = name.substring(name.lastIndexOf("."));
+            }
+        } catch (Exception e) {
+            extension = " ";
+        }
+
+        return extension;
+
+    }*/
 }
 
